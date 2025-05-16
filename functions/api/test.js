b4e39eb04286ede8cc1e89a1a95c151d76906633
goukey <goukey@sina.com>
2025-05-16 10:48:38 +0800

    清理无效文件

--- functions/api/test.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-  return new Response("Hello World", {
-    headers: {
-      "Content-Type": "text/plain;charset=UTF-8",
-      "Access-Control-Allow-Origin": "*"
-    }
-  });
-} 
